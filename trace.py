580016fffa3e2085ed16f584447eb0c076109e5e
Frenkits <101058801+CrizzyMan@users.noreply.github.com>
2023-01-24 07:02:42 -0500

    Add files via upload

---
+++ trace.py
@@ -0,0 +1,18 @@
+import geocoder
+import folium
+
+g = geocoder.ip("ip-address")
+
+myAddress = g.latlng
+print(myAddress)
+
+my_map1 = folium.Map(location=myAddress,
+                     zoom_start=12)
+
+folium.CircleMarker(location=myAddress,
+                    radius=50, popup="Trovato").add_to(my_map1)
+
+folium.Marker(myAddress,
+              popup="Trovato").add_to(my_map1)
+
+my_map1.save("my_map.html ")
